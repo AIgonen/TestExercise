b9588ade2df71ef340792d2ea0dcafe1d5b4de3e
Andrei <a.igonen@gmail>
2022-01-13 00:37:30 +0200

    update

---
+++ TestExercise/src/testexercise/Result.java
@@ -0,0 +1,37 @@
+package testexercise;
+
+public class Result {
+    
+    public double dayTimeTotal;
+    public double nightTimeTotal;
+
+    public Result(double dayTimeTotal, double nightTimeTotal) {
+        this.dayTimeTotal = dayTimeTotal;
+        this.nightTimeTotal = nightTimeTotal;
+    }
+
+    public double getDayTimeTotal() {
+        return dayTimeTotal;
+    }
+
+    public void setDayTimeTotal(double dayTimeTotal) {
+        this.dayTimeTotal = dayTimeTotal;
+    }
+
+    public double getNightTimeTotal() {
+        return nightTimeTotal;
+    }
+
+    public void setNightTimeTotal(double nightTimeTotal) {
+        this.nightTimeTotal = nightTimeTotal;
+    }
+
+    @Override
+    public String toString() {
+        return "päev: " + dayTimeTotal + ", öö: " + nightTimeTotal;
+    }
+
+    
+
+    
+}
--- TestExercise/src/testexercise/TestExercise.java
+++ TestExercise/src/testexercise/TestExercise.java
@@ -1,123 +1,80 @@
 
 package testexercise;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;  
 import java.time.Duration;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.Period;
+import java.time.temporal.ChronoUnit;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Scanner;
 
 
+
 public class TestExercise {
-
-
-    public static void main(String[] args) {
+        
+    public static void main(String[] args) throws ParseException {
         
         Scanner scn = new Scanner(System.in);
         
-        LocalTime startTime;
-        LocalTime endTime;
+        SimpleDateFormat formatter = new SimpleDateFormat("HH:mm"); 
         
+        String p1 = "21:00"; 
+        String p2 = "08:00"; 
+
+        Date startTime = formatter.parse(p1);
+        Date endTime = formatter.parse(p2);
+            
+        if (endTime.before(startTime)) {
+            Calendar cal = Calendar.getInstance();
+            cal.setTime(endTime);
+            cal.add(Calendar.DAY_OF_MONTH, 1);
+            endTime = cal.getTime();
+        }
+            
+        
+        Result total = CalculatePeriods(startTime, endTime);
+        System.out.println(total.toString());
+        
+    }
+    
+    private static Result CalculatePeriods(Date startTime, Date endTime) throws ParseException{
+        SimpleDateFormat formatter = new SimpleDateFormat("HH:mm"); 
+ //       DateTimeFormatter timeFormater = DateTimeFormatter.ofPattern("HH:mm");
+        
+        Date MorningHoursStart = formatter.parse("05:59");
+        Date NightHoursStart = formatter.parse("21:59");
+            
+        int dayTime = 0;
+        int nightTime = 0;
+        
+        double dayHours = 0;
+        double nightHours = 0;
+        
+        Date step = formatter.parse("00:15");
+
+            while (startTime.before(endTime))
+            {
+                if (startTime.after(MorningHoursStart) && startTime.before(NightHoursStart)) {
+                    dayTime += 15;
+                } else{
+                    nightTime += 15;
+                }
                 
-        System.out.println("siseskage kellaajad formaadis \"HH:mm\"");
-        
-        try{
-            System.out.println("Sisestage alguskellaaeg");
-            startTime = LocalTime.parse(scn.next());   
+                Calendar cal = Calendar.getInstance();
+                cal.setTime(startTime);
+                cal.add(Calendar.MINUTE, 15);
+                startTime = cal.getTime();
+            }
             
-            System.out.println("Sisestage lõppkellaaeg");
-            endTime = LocalTime.parse(scn.next());
-            
-            double[] times = GetTimes(startTime, endTime); // inserting in to array a ammount of hours at day and ammount of hours at night 
-            PrintTimes(times, startTime, endTime); //printing amount of hours to a console
-        } catch(Exception e){
-            System.out.println("Kellaeg on sisestatud valesti!");
+            dayHours = dayTime / 60.0;
+            nightHours = nightTime / 60.0;
+        
+            Result result = new Result(dayHours, nightHours);
+            return result;
         }
-  
-    }
-    
-    /**
-    * Prints startTime, endTime and amount of hours to a console.
-    */
-    public static void PrintTimes(double[] times, LocalTime startTime, LocalTime endTime){
-        System.out.println("Algus: " + startTime + ", lõpp: " + endTime);
-        
-        System.out.println("Öö\t " + times[1]);
-        System.out.println("Päev\t " + times[0]);
-    }
-
-    
-    /**
-    * gets amount of night and day hours .
-    */
-    public static double[] GetTimes(LocalTime startTime,  LocalTime endTime){
-
-        double firstHours, secondHours;         
-        
-        firstHours = (double)findDayTime(startTime,  endTime) / 3600;
-        secondHours = (double)findNightTime(startTime,  endTime) / 3600;
-        
-        double[] times = {firstHours, secondHours};
-
-        return times;
-    }
-        
-    /**
-    * finds amount of daytime hours.
-    */
-    public static long findDayTime(LocalTime startTime,  LocalTime endTime){
-        long totalSeconds;        
-        LocalTime MorningHoursStart = LocalTime.parse("06:00");
-        LocalTime NightHoursStart = LocalTime.parse("22:00");
-        
-        // if starttime and endtime are both in daytime 
-        if(startTime.getHour() < 6 && endTime.getHour() < 6 && startTime.getHour() < endTime.getHour()){
-            totalSeconds = 0;
-        } else if (startTime.getHour() >= 6 && endTime.getHour() < 6){ // stratTime after 6.00, endTime before 06.00
-            Duration duration = Duration.between(startTime, NightHoursStart);
-            totalSeconds = duration.getSeconds();                
-        } else if (startTime.getHour() >= 6 && endTime.getHour()<= 22){ // 6.00 - 22.00
-            Duration duration = Duration.between(startTime, endTime);
-            totalSeconds = duration.getSeconds();
-        } else if (startTime.getHour() >= 6 && endTime.getHour() > 22){ // stratTime after 6.00, endTime before 22.00
-            Duration duration = Duration.between(startTime, NightHoursStart);
-            totalSeconds = duration.getSeconds();
-        } else if (startTime.getHour() < 6 && endTime.getHour() < 6 ){ // stratTime before 6.00, endTime after 00.00
-            totalSeconds = 57600;
-        } /*else if (startTime.getHour() < 6 && endTime.getHour() < 6){ // stratTime before 6.00, endTime before 06.00
-        totalSeconds = 0;
-        }*/ else if (startTime.getHour() < 6 && endTime.getHour() < 22 ){ // stratTime before 6.00, endTime before 22.00
-            Duration duration = Duration.between(MorningHoursStart, endTime);
-            totalSeconds = duration.getSeconds();
-        } else if (startTime.getHour() < 6 && endTime.getHour() > 22){ // stratTime before 6.00, endTime after 22.00
-            totalSeconds = 57600; // 16h
-        } else 
-            totalSeconds = 57600; // 16h
-    return totalSeconds;    
-    }
-    
-    /**
-    * founds amount of nighttime hours .
-    */
-    public static long findNightTime(LocalTime startTime,  LocalTime endTime){
-        long totalSeconds;      
-        
-        LocalTime MorningHoursStart = LocalTime.parse("06:00");
-        LocalTime NightHoursStart = LocalTime.parse("22:00");
-        LocalTime midnight = LocalTime.parse("00:00");
-        
-        if(startTime.getHour() < 6 && endTime.getHour() < 6 && startTime.getHour() < endTime.getHour()){
-            Duration duration = Duration.between(startTime, endTime);
-            totalSeconds = duration.getSeconds();
-        } else if (startTime.getHour() < 6 && endTime.getHour() < 6 ){ // stratTime before 6.00, endTime after 00.00
-            Duration duration = Duration.between(startTime, endTime);
-            totalSeconds = 28800 + duration.getSeconds();
-        } else if (startTime.getHour() >= 6 && endTime.getHour() < 6){ // stratTime after 6.00, endTime after 00.00
-            Duration duration = Duration.between(midnight, endTime);
-            totalSeconds = duration.getSeconds() + 7200;
-        } else {
-            Duration duration = Duration.between(startTime, endTime);
-            totalSeconds = duration.getSeconds() - findDayTime(startTime, endTime);
-        }
-        return totalSeconds;
-    }     
 }
